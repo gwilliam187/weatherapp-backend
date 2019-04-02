ecc8086d8655dacc6408dab0731d471b35a9f6f7
gwilliam187 <33857496+gwilliam187@users.noreply.github.com>
2019-04-02 11:04:59 +0200

    added more actions and reducers

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,5 @@
+// User Actions
+export const SET_USERS = 'SET_USERS';
+
+// Selected User Actions
 export const SELECT_USER = 'SELECT_USER';
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,5 +1,12 @@
+import { SET_USERS } from '../actions/actionTypes';
+
 const initialState = ['steven_klarens', 'wowowi', 'sandiaga_uno'];
 
-export default(state = [], action) => {
-	return initialState;
+export default(state = initialState, action) => {
+	switch(action.type) {
+		case SET_USERS:
+			return action.payload;
+		default:
+			return state;
+	}
 }
